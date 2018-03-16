84e049f56ff921a983ee63a311cda8bb86100951
yDeepak1889 <iit2015124@iiita.ac.in>
2018-03-17 04:39:02 +0530

    contract address error resolved

--- app/run.js
+++ app/run.js
@@ -1,9 +1,6 @@
 Web3 = require('web3')
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-<<<<<<< HEAD
-=======
 var fs = require("fs")
->>>>>>> 44db8e24dfac81c37b1d1e17aa1caf95f3a2d20a
 code = fs.readFileSync('../contracts/Voting.sol').toString()
 solc = require('solc')
 compiledCode = solc.compile(code)
@@ -11,5 +8,8 @@ abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
 VotingContract = web3.eth.contract(abiDefinition)
 byteCode = compiledCode.contracts[':Voting'].bytecode
 deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
-deployedContract.address
+setTimeout(function () {
+var a = deployedContract.address
+console.log(a)
 contractInstance = VotingContract.at(deployedContract.address)
+}, 1000)
