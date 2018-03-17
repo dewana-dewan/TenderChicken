ab114dbc905895ac5d1594b704d9144a5ec8d05b
Aditya Dewan <iit2015097@iiita.ac.in>
2018-03-17 16:41:23 +0530

    struggling with sql and data types

---
+++ n_app/make_db.sql
@@ -0,0 +1,2 @@
+use database test_hint;
+create table tenders(tenderId INT(10), address VARCHAR(45), abi VARCHAR(10000), byteCode VARCHAR(10000), link VARCHAR(10000));
--- n_app/run.js
+++ n_app/run.js
@@ -1,13 +1,41 @@
 const express        = require('express');
 var router           = express.Router();
-const MongoClient    = require('mongodb').MongoClient;
+// const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+// var mongoose = require('mongoose');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "codebuildhack",
+  database: "hint_test"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+var glo_hash         = {};
 
 Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var fs = require("fs")
-solc = require('solc')
+var fs = require("fs");
+solc = require('solc');
+
+
+// mongoose.connect('mongodb+srv://dewan:dewan123@hint1-aytbe.mongodb.net/test')
+// var TenderSchema = new mongoose.Schema({  
+//   name: String,
+//   address: String,
+//   abi: String,
+//   bytecode: String,
+//   link: String
+// });
+// mongoose.model('Tender', TenderSchema);
+// module.exports = mongoose.model('Tender');
 
 var port = process.env.PORT || 3000;
 
@@ -19,20 +47,42 @@ app.use('/api', router);
 
 router.get('/make_contract', function(req, res) {
 
-	console.log(req.query.name);
+	var id_name = req.query.name
+	console.log(id_name);
 
 	code = fs.readFileSync('./contracts/Voting.sol').toString()
 	compiledCode = solc.compile(code)
-	abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
+	abipre = compiledCode.contracts[':Voting'].interface;
+	abiDefinition = JSON.parse(abipre)
 	VotingContract = web3.eth.contract(abiDefinition)
 	byteCode = compiledCode.contracts[':Voting'].bytecode
 	deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
 
 	setTimeout(function () {
 		var a = deployedContract.address
+		var link = 'test_link';
 		console.log(a)
-		contractInstance = VotingContract.at(deployedContract.address)
-    	res.json({ message: 'hooray!', address: a });   
-	}, 1000)
-	
+		contractInstance = VotingContract.at(deployedContract.address);
+		// console.log(typeof(abipre), abipre, abipre.toString());
+		var insert_sql = 'insert into tenders (tenderId , address , link) values ?';
+		var new_obj = [id_name, a, link];
+		console.log(new_obj);
+		con.query(insert_sql, [new_obj], function (err, result) {
+		    if (err) throw err;
+		    console.log("Number of records inserted: " + result.affectedRows);
+		  });
+		// Tender.create({
+		// 	  name: id_name,
+		// 	  address: a,
+		// 	  abi: JSON.parse(compiledCode.contracts[':Voting'].interface),
+		// 	  bytecode: byteCode,
+		// 	  link: 'String'
+		// })
+		glo_hash[req.query.name] = {
+							address: a, 
+							abiDefinition: JSON.parse(compiledCode.contracts[':Voting'].interface), 
+							bytecode: byteCode
+						};
+    	res.json(glo_hash[req.query.name]);   
+	}, 1000);
 });
