a129f5859239a21f615cc097f5c73cbbe1b5a6e8
Aditya Dewan <iit2015097@iiita.ac.in>
2018-03-17 06:19:47 +0530

    remove all unnecessary starter code

--- n_app/run.js
+++ n_app/run.js
@@ -1,15 +1,16 @@
 Web3 = require('web3')
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var fs = require("fs")
-code = fs.readFileSync('../contracts/Voting.sol').toString()
+code = fs.readFileSync('./contracts/Voting.sol').toString()
 solc = require('solc')
 compiledCode = solc.compile(code)
 abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
 VotingContract = web3.eth.contract(abiDefinition)
 byteCode = compiledCode.contracts[':Voting'].bytecode
 deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
+
 setTimeout(function () {
-var a = deployedContract.address
-console.log(a)
-contractInstance = VotingContract.at(deployedContract.address)
+	var a = deployedContract.address
+	console.log(a)
+	contractInstance = VotingContract.at(deployedContract.address)
 }, 1000)
