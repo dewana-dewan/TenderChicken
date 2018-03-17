190aa5b6951bf09d8ac7bd9feae5bcddae7a829a
Aditya Dewan <iit2015097@iiita.ac.in>
2018-03-17 07:39:40 +0530

    api for contract creation in progress

--- n_app/run.js
+++ n_app/run.js
@@ -1,16 +1,38 @@
-Web3 = require('web3')
+const express        = require('express');
+var router           = express.Router();
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+
+Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var fs = require("fs")
-code = fs.readFileSync('./contracts/Voting.sol').toString()
 solc = require('solc')
-compiledCode = solc.compile(code)
-abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
-VotingContract = web3.eth.contract(abiDefinition)
-byteCode = compiledCode.contracts[':Voting'].bytecode
-deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
 
-setTimeout(function () {
-	var a = deployedContract.address
-	console.log(a)
-	contractInstance = VotingContract.at(deployedContract.address)
-}, 1000)
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + port);
+});
+
+app.use('/api', router);
+
+router.get('/make_contract', function(req, res) {
+
+	console.log(req.query.name);
+
+	code = fs.readFileSync('./contracts/Voting.sol').toString()
+	compiledCode = solc.compile(code)
+	abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
+	VotingContract = web3.eth.contract(abiDefinition)
+	byteCode = compiledCode.contracts[':Voting'].bytecode
+	deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
+
+	setTimeout(function () {
+		var a = deployedContract.address
+		console.log(a)
+		contractInstance = VotingContract.at(deployedContract.address)
+    	res.json({ message: 'hooray!', address: a });   
+	}, 1000)
+	
+});
